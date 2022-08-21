4a7e9a497db8ec3bb3e47d6934af4689b0c75819
Victor Emmanuel <victorpassos2012@gmail.com>
2022-08-21 16:51:59 -0300

    Requisito 1 feito

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,6 +1,26 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from ..factories.product_factory import ProductFactory
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
-# teste aqui :p
+    fake_product = ProductFactory()
+    product = Product(
+        fake_product.id,
+        fake_product.nome_do_produto,
+        fake_product.nome_da_empresa,
+        fake_product.data_de_fabricacao,
+        fake_product.data_de_validade,
+        fake_product.numero_de_serie,
+        fake_product.instrucoes_de_armazenamento,
+    )
+
+    assert product.id == fake_product.id
+    assert product.nome_do_produto == fake_product.nome_do_produto
+    assert product.nome_da_empresa == fake_product.nome_da_empresa
+    assert product.data_de_fabricacao == fake_product.data_de_fabricacao
+    assert product.data_de_validade == fake_product.data_de_validade
+    assert product.numero_de_serie == fake_product.numero_de_serie
+    assert (
+        product.instrucoes_de_armazenamento
+        == fake_product.instrucoes_de_armazenamento
+    )
